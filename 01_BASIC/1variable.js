b91225cc9be45f69f4c468e8bdbfdebe68c34780
neelesh18904 <neelesh18903@gmail.com>
2025-02-22 18:27:57 +0000

    variable and datatype

---
+++ 01_BASIC/1variable.js
@@ -0,0 +1,17 @@
+const accountId=1443
+let accountEmail="neelesh70079@gmail.com"
+var accountPassword="123456"
+accountCity="jaipur"
+
+// accountId=2// not allowed
+
+accountEmail="nk@gmail.com"
+accountPassword="227813"
+accountCity="bengaluru"
+let accountState;
+
+console.log(accountId);
+
+/*prefer not to use var beacuse of issues in block scope and function scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_BASIC/2datatype.js
@@ -0,0 +1,28 @@
+"use strict";//treat all js code as newer version//
+// alert(3+3)//we are using nodejs not browser/
+console.log(3
+    +
+    3)  // code readibility should be high//
+
+
+    let name="neelesh" // string datatye:
+    let age=18 // number datatype//
+    let isloogedin=false // boolean//
+
+
+   //primitive data type//
+    //number--range--2 to the power of 53//
+    //bigint//
+    //string we can use '',""//
+    //boolean=> true/false//
+    //null=>standlone value null representation hai ek empty value ka (let state=null) it means wo undefined nhi hai khali hai //
+    //undefined when value not defined variable is declared but value not defined//
+    //symbol => componenet to identifies uniqueness ke liye use//
+
+    //object
+
+    console.log(typeof "neelesh")
+    console.log(typeof age)
+    console.log(typeof null) // null ek object hai//
+    console.log(typeof undefined) // undefined type//
+
