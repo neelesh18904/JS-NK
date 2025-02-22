7f676e93ef72fbe1b5cc78ca62f1a6ab1c6e05a2
neelesh18904 <neelesh18903@gmail.com>
2025-02-22 19:09:39 +0000

    conversion

--- 01_BASIC/3conversionoperation.js
+++ 01_BASIC/3conversionoperation.js
@@ -8,10 +8,10 @@
 // console.log(value);
 
 // let isloogedin=1;
-let isloogedin = "neelesh"  // empty string  give false//
+// let isloogedin = "neelesh"  // empty string  give false//
 
-let booleanIsLoggedIn=Boolean(isloogedin)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isloogedin)
+// console.log(booleanIsLoggedIn);
 
 
 
@@ -23,9 +23,60 @@ console.log(booleanIsLoggedIn);
 //"neelesh"=>true//
 
 // now learn about cheize string ke andar kaise convert//
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//******************operation************************//
+let value=3;
+let negvalue=-value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+// string add//
+// let str1="heelo"
+// let str2=" Neelesh"
+// let str3=str1+str2;
+// console.log(str3);
+
+//problem//
+// console.log("1"+2);// yaha pe 3 ana chahiye tha//
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"4");
+
+// console.log(true);//true
+// console.log(+true); //1//
+// console.log(+"");
+
+// let num1,num2,num3
+// num1=num2=num3=2+2  better way me banao readibility
+
+let gamecounter=100;
+gamecounter++; //postfix use ke baad value increases
+++gamecounter; // pahle value increses//
+console.log(gamecounter);
+
+// go documnetation//
+
+//link to study//
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
 
 
