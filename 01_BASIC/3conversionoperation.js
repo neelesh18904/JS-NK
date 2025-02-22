c395f7e68f8bf842f5cedc0da430f20c1899c541
neelesh18904 <neelesh18903@gmail.com>
2025-02-22 18:54:10 +0000

    conversion

---
+++ 01_BASIC/3conversionoperation.js
@@ -0,0 +1,31 @@
+// let score="33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let value=Number(score);
+// //conversion//
+// console.log(typeof value);
+// console.log(value);
+
+// let isloogedin=1;
+let isloogedin = "neelesh"  // empty string  give false//
+
+let booleanIsLoggedIn=Boolean(isloogedin)
+console.log(booleanIsLoggedIn);
+
+
+
+// "33" jab ek number ko convert karte hai 33 eassily convert//
+// "33abc" not convert => value give NaN=Not an Number (type is NUMBER)//
+// "true"=>1; false=>0;
+//"1"=>true; 0=>false
+// ""=>false//
+//"neelesh"=>true//
+
+// now learn about cheize string ke andar kaise convert//
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
