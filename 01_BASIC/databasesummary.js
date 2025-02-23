e3b8b0168d86f08c5e46f276ecf18b77e33df8f7
neelesh18904 <neelesh18903@gmail.com>
2025-02-23 09:51:21 +0000

    Summary

--- 01_BASIC/databasesummary.js
+++ 01_BASIC/databasesummary.js
@@ -43,3 +43,28 @@ console.log(typeof myobj);
 //Array=>objec
 //Function=>function(object function)
 //Object=>object
+
+
+//******Memaory *///
+//Stack Meamory uesed byPrimitive datatype)-->when stack meamory used it means jo bhi variable declared kiya uski copy milti hai
+//Heap Meamory used by Non primitive datatype//-->when variable or object define in heap then refernece (original v me change)
+
+let myyoutubename="Neelesh Kumar"
+let anothername=myyoutubename
+anothername="shubham"
+console.log(anothername);
+console.log(myyoutubename);
+
+let userone={
+    email:"neelesh18903@gmail.com",
+    upid:"usert@ybl"
+}
+let usertwo=userone
+usertwo.email="shubham123@gmail.com"
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
+
+
